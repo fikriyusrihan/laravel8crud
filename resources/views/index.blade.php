36d601b2dc3525bd313e20c81a2e73f10ccdde3e
Fikri Yusrihan <yusrihanfikri@gmail.com>
2021-01-25 20:24:03 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layout')
+
+@section('content')
+    <style>
+        .uper {
+            margin-top: 40px;
+        }
+        .btn-add {
+            margin-bottom: 24px;
+        }
+    </style>
+    <div class="uper">
+        @if (session()->get('success'))
+            <div class="alert alert-success">
+                {{ session()->get('success') }}
+            </div>
+        @endif
+        <a href="{{ route('games.create') }}" class="btn btn-primary btn-add">Add Games</a>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Game Name</td>
+                    <td>Game Price</td>
+                    <td colspan="2">Action</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($games as $game)
+                    <tr>
+                        <td>{{ $game->id }}</td>
+                        <td>{{ $game->name }}</td>
+                        <td>{{ $game->price }}</td>
+                        <td><a href="{{ route('games.edit', $game->id) }}" class="btn btn-primary">Edit</a></td>
+                        <td>
+                            <form action="{{ route('games.destroy', $game->id) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger" type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
